ea0b6db4386a7de0b492cb17eb5a2a9d7f80f7f0
Bruno Salmito <brunosalmito@gmail.com>
2020-04-10 07:27:21 -0300

    correção ainate

--- js/animate.js
+++ js/animate.js
@@ -1,3 +1,19 @@
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
 const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
@@ -9,13 +25,14 @@ function animeScroll() {
             el.classList.add('animate');
 
         } else {
-
+            el.classList.remove('animate');
         }
     })
-
-
 }
 
-window.addEventListener('scroll', function () {
-    animeScroll();
-})
+animeScroll();
+if (target.length) {
+    window.addEventListener('scroll', debounce(function () {
+        animeScroll();
+    }, 200))
+}
